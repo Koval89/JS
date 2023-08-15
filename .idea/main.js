1cb5e589c8b2f44fe6164c5efc45095e751ea8ba
User <alexandrgalaxy4@gmail.com>
2023-08-15 23:41:10 +0300

    1

---
+++ .idea/main.js
@@ -0,0 +1,52 @@
+let a1;
+a1='hellow'
+console.log(a1)
+let a2;
+a2='owu'
+console.log(a2)
+let a3;
+a3='com'
+console.log(a3)
+let a4;
+a4='ua'
+console.log(a4)
+let b1=1;
+console.log(b1)
+let b2=10;
+console.log(b2)
+let b3=-999;
+console.log(b3)
+let b4=123;
+console.log(b4)
+let b5=3.14;
+console.log(b5)
+let b6=2.7;
+console.log(b6)
+let b7=17;
+console.log(b7)
+let a5;
+a5='true'
+console.log(a5)
+let a6;
+a6='false';
+console.log(a6)
+let user={
+    firstName:'igor',
+    middleName:'Ivanovich',
+    lastName:'Goroh'
+};
+console.log(user)
+let arr=['igor','Ivanovich','Goroh']
+console.log(arr); ['igor','Ivanovich','Goroh']
+console.log(arr[2]);
+console.log(arr[0])
+console.log(arr[1])
+
+
+let a = 100;
+let b = '100';
+let c = true
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+
