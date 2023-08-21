548bed4fc8875c575f311a46a4ab1e8dd337b0a0
User <alexandrgalaxy4@gmail.com>
2023-08-21 16:14:39 +0300

    1

--- .idea/HW 2/main.js
+++ .idea/HW 2/main.js
@@ -29,9 +29,9 @@
 // console.log(boocks)
 
 // let boocks=[
-//     {title:'The Little Prince',pageCount: 100,genre:'story',authors:['A. de Saint-Exupéry']},
-//     {title: 'Red sails',pageCount: 70, genre: 'A story extravaganza',authors: ['A. Green']},
-//     {title: 'Three musketeers',pageCount: 95,genre: 'novel',authors: ['A. Dumas']}
+//     {title:'The Little Prince',pageCount: 100,genre:'story',authors:{name:'A. de Saint-Exupéry', age:55}},
+//     {title: 'Red sails',pageCount: 70, genre: 'A story extravaganza',authors: {name:'A. Green', age:48}},
+//     {title: 'Three musketeers',pageCount: 95,genre: 'novel',authors: {name:'A. Dumas', age:39}}
 // ]
 // console.log(boocks)
 
@@ -151,5 +151,57 @@
 //         console.log('Saturday');
 //         break;
 //     case 7:
-//         console.log('Sunday')
+//         console.log('Sunday');
+//     default :
+//         console.log(Error)
 // }
+
+
+// - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
+//     за допомоги  оператора || буде присвоювати змінній х значення "default"  якщо значення змінної х являється falsy (хибноподібні, тобто приводиться до false)
+
+// let x =prompt('any value') || "default";
+// console.log(x);
+
+// - з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на тривалість навчання. У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
+
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// console.log(coursesAndDurationArray)
+// // let coursesAndDurationArray=prompt()
+// if (coursesAndDurationArray[0].monthDuration >=5){
+//     console.log('Super')
+// }else {
+//     console.log('not great')
+// };
+// if (coursesAndDurationArray[1].monthDuration >=5){
+//     console.log('Super')
+// }else {
+//     console.log('not great')
+// };
+// if (coursesAndDurationArray[2].monthDuration >=5){
+//     console.log('Super')
+// }else {
+//     console.log('not great')
+// };
+// if (coursesAndDurationArray[3].monthDuration >=5){
+//     console.log('Super')
+// }else {
+//     console.log('not great')
+// };
+// if (coursesAndDurationArray[4].monthDuration >=5){
+//     console.log('Super')
+// }else {
+//     console.log('not great')
+// };
+// if (coursesAndDurationArray[5].monthDuration >=5){
+//     console.log('Super')
+// }else {
+//     console.log('not great')
+// };
