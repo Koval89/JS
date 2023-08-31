726a161d2984aee5d384a079b3b1706485e4d12d
User <alexandrgalaxy4@gmail.com>
2023-08-31 19:58:25 +0300

    1

--- .idea/HW 5/main.js
+++ .idea/HW 5/main.js
@@ -20,14 +20,31 @@
 // - створити функцію яка приймає масив та виводить кожен його елемент
 
 
-// let item = () => console.log([25,36,45,'petya','olya'])
-// item()
+// const someItem= (arr) => {
+//     for(const item of arr){
+//         console.log('item:', item)
+//     }
+// }
+// someItem([25,36,150,'petya', 'olya'])
+
+// - створити функцію яка створює параграф з текстом. Текст задати через аргумент
+
+// const writer = (number,carBrand,status) =>{
+//     document.write(
+//         `<div>
+//             <h1>${number} ${carBrand}</h1>
+//             <p>${status}</p>
+//         </div>`
+//     );
+// }
+// writer(1, 'BMW', 'sale')
+// writer(2,'audi', 'notForSale')
 
 
 
 // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
 
-// let writer = (product) =>
+// const writer = (product) =>{
 //     document.write(
 //         `<ul>
 //         <li>${product}</li>
@@ -35,16 +52,26 @@
 //         <li>${product}</li>
 //         </ul> `
 //     )
+// }
 // writer('coffee')
+
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+
+// const writer1 = (product, number) =>{
+//     document.write(`<ul>`);
+//     for (let i=0;i < number; i++){
+//     document.write(`<li>${product}</li>`);
+//     }
+//     document.write(`</ul>`);
+// }
+// writer1('coffee 1', 6)
+// writer1('milk 2', 3)
+
 
 
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
 
-
-
-
-
-// function printerArray(array) {
+// const printerArray = (array) => {
 //     for (const item of array) {
 //         console.log(item);
 //     }
@@ -58,15 +85,6 @@
 //     {brand: 'lada', age: 1977, status: true},
 // ];
 // printerArray(cars);
-//
-// // const printer = (array)  => {
-// //     if (array.length){
-// //         for (const item of array){
-// //             console.log(item)
-// //         }
-// //     }
-// // }
-// // printer(cars)
 
 
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
@@ -81,7 +99,7 @@
 //     {name: 'anya', age: 31, id: 6},
 //
 // ];
-// function printer(array){
+// const printer = (array) =>{
 //     for (const users of array){
 //         document.write(`
 //         <div>
@@ -93,33 +111,60 @@
 //     }
 // }
 // printer(users)
-//
-// const print =(array) => {
-//     for (const item of array){
-//         console.log(item)
+
+
+
+// створити функцію яка повертає найменьше число з масиву
+
+// const min = (arr)=>{
+//     let min = arr[0];
+//     for (let i = 1; i < arr.length; i++){
+//         if(arr[i] < min){
+//             min=arr[i];
+//         }
 //     }
+//     return min;
+//
 // }
-// print(users)
-
-
+// const  result = min ([1,55,25,13,56,-10])
+// console.log('number', result)
 
 // - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
 
-// function sum(){
-//     const arr = Array.from(arguments)
-//     let result = 0
-//     for (const number of arr){
-// result = result + number
-//     }
-//     return result;
-//
-// }
-// console.log(sum(1,2,10))
-// console.log(sum(22,11,35))
-//
-// const sum1 = (num1, num2, num3) => {
-//     return num1+num2+num3;
-// }
-// console.log(sum1(1,2,10))
-// console.log(sum1(22,11,35))
-//
+const sum = (arr) =>{
+    let result = 0
+    for (let i = 0; i < arr.length; i++){
+result = result + arr[i]
+    }
+    return result;
+
+}
+
+console.log(sum([1,2,10]))
+console.log(sum([22,11,35]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
